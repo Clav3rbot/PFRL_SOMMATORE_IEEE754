8a9eb3a26bf24d850e9b136a13e1ce1547fb9e8c
Domenico <domenico_pittari@mail.polimi.it>
2023-08-02 17:42:58 +0200

    Addeed some tests

--- TB_PRE_SUM.vhd
+++ TB_PRE_SUM.vhd
@@ -8,61 +8,82 @@ END TB_PRE_SUM;
 
 ARCHITECTURE behavior OF TB_PRE_SUM IS
 
-        -- Component Declaration
-        COMPONENT PRE_SUM
-                PORT (
-                        X : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-                        Y : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-                        OP : IN STD_LOGIC;
-                        XSIGN : OUT STD_LOGIC;
-                        YSIGN : OUT STD_LOGIC;
-                        XEXP : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-                        XMAN : OUT STD_LOGIC_VECTOR (23 DOWNTO 0);
-                        YMAN : OUT STD_LOGIC_VECTOR (23 DOWNTO 0);
-                        XCASE : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-                        YCASE : OUT STD_LOGIC_VECTOR (2 DOWNTO 0)
-                );
-        END COMPONENT;
+    -- Component Declaration
+    COMPONENT PRE_SUM
+        PORT (
+            X : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+            Y : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+            OP : IN STD_LOGIC;
+            XSIGN : OUT STD_LOGIC;
+            YSIGN : OUT STD_LOGIC;
+            XEXP : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
+            XMAN : OUT STD_LOGIC_VECTOR (23 DOWNTO 0);
+            YMAN : OUT STD_LOGIC_VECTOR (23 DOWNTO 0);
+            XCASE : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
+            YCASE : OUT STD_LOGIC_VECTOR (2 DOWNTO 0)
+        );
+    END COMPONENT;
 
-        --Inputs
-        SIGNAL X : STD_LOGIC_VECTOR(31 DOWNTO 0);
-        SIGNAL Y : STD_LOGIC_VECTOR(31 DOWNTO 0);
-        SIGNAL op : STD_LOGIC;
+    --Inputs
+    SIGNAL X : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL Y : STD_LOGIC_VECTOR(31 DOWNTO 0);
+    SIGNAL op : STD_LOGIC;
 
-        --Outputs
-        SIGNAL XSIGN : STD_LOGIC;
-        SIGNAL YSIGN : STD_LOGIC;
-        SIGNAL XEXP : STD_LOGIC_VECTOR(7 DOWNTO 0);
-        SIGNAL XMAN : STD_LOGIC_VECTOR(23 DOWNTO 0);
-        SIGNAL YMAN : STD_LOGIC_VECTOR(23 DOWNTO 0);
-        SIGNAL XCASE : STD_LOGIC_VECTOR(2 DOWNTO 0);
-        SIGNAL YCASE : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    --Outputs
+    SIGNAL XSIGN : STD_LOGIC;
+    SIGNAL YSIGN : STD_LOGIC;
+    SIGNAL XEXP : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL XMAN : STD_LOGIC_VECTOR(23 DOWNTO 0);
+    SIGNAL YMAN : STD_LOGIC_VECTOR(23 DOWNTO 0);
+    SIGNAL XCASE : STD_LOGIC_VECTOR(2 DOWNTO 0);
+    SIGNAL YCASE : STD_LOGIC_VECTOR(2 DOWNTO 0);
+	 
 BEGIN
 
-        -- Component Instantiation
-        uut : PRE_SUM PORT MAP(
-                X => X,
-                Y => Y,
-                OP => OP,
-                XSIGN => XSIGN,
-                YSIGN => YSIGN,
-                XEXP => XEXP,
-                XMAN => XMAN,
-                YMAN => YMAN,
-                XCASE => XCASE,
-                YCASE => YCASE
-        );
-        --  Test Bench Statements
-        tb : PROCESS
-        BEGIN
+    -- Component Instantiation
+    uut : PRE_SUM PORT MAP(
+        X => X,
+        Y => Y,
+        OP => OP,
+        XSIGN => XSIGN,
+        YSIGN => YSIGN,
+        XEXP => XEXP,
+        XMAN => XMAN,
+        YMAN => YMAN,
+        XCASE => XCASE,
+        YCASE => YCASE
+    );
+	
+      -- Stimulus process
+   stim_proc: process
+   begin	
 
-                WAIT FOR 100 ns;
+      wait for 100 ns;
+			X <= "00010000100000000000000000000000";
+			Y <= "00010000100000000000000000000001";
+			op	 <= '0';
+      wait for 100 ns;
+    		X <= "01000001111010001100110011000000";
+			Y <= "01000001111010001100110011000001";
+			op	 <= '0';
+      wait for 100 ns;
+    		X <= "01000001111010001100110011000000";
+			Y <= "11000001111010001100110011000000";
+			op	 <= '0';
+		wait for 100 ns;
+    		X <= "01000001111010001100110011000000";
+			Y <= "11000001111010001100110011000000";
+			op	 <= '1';
+		wait for 100 ns;
+    		X <= "01000001111010001100110011000000";
+			Y <= "11111111111010001100110011000000";
+			op	 <= '1';
+		wait for 100 ns;
+    		X <= "01000001111010001100110011000000";
+			Y <= "11111111111010001100110011000000";
+			op	 <= '1';
+		wait;
 
-                X <= "00010000100000000000000000000000";
-                Y <= "00010000100000000000000000000001";
-                OP <= '0';
-                WAIT;
-        END PROCESS tb;
-        --  End Test Bench 
+    END process;
 
 END;
